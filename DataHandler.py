589f0d23d49fa9102dbec60311bac033e949f7f1
johncanthony <john.claud.anthony@gmail.com>
2019-05-19 17:17:32 -0700

    Added more CRUD operations to ImgManifest

--- DataHandler.py
+++ DataHandler.py
@@ -1,15 +1,81 @@
 import os
 import json
-
+import hashlib
 
 class ImgManifest():
 
 
     _CONFIG_PATH='CONFIG'
+    _IMG_PATH='IMG'
 
     def __init__(self,idstr):
 
         self._id = idstr
+        self._filename="{}/{}.json".format(self._CONFIG_PATH,self._id)
+
+    def __str__(self):
+        return str(self.read())
+
+
+    def _checksum(self,data):
+        chkString = ""
+
+        for entry in data:
+            chkString+=str(entry)
+
+        return hashlib.md5(chkString).hexdigest()
+
+    '''
+    Reads the contents of the manifest file
+    '''
+    def read(self):
+
+        data=None
+
+        if self._manifest_exists(self._filename):
+            try:
+                with open(self._filename,'r') as manifest:
+                    data = json.load(manifest)
+
+            except EnvironmentError:
+                pass
+
+        return data
+
+
+    '''
+    Writes data to the specified file
+    '''
+    def write(self,data):
+
+        filename = None
+
+        try:
+            with open(self._filename,'w') as manifest:
+                json.dump(data,manifest)
+            filename = self._filename
+        except EnvrionmentError:
+            pass
+
+        return filename
+
+
+    def add_img(self,img_filename):
+
+        if not self._manifest_exists(self._filename):
+            filename=self.create()
+
+        manifest = self.read()
+
+        img_path = "{}/{}".format(self._IMG_PATH,img_filename);
+
+        if(manifest['data'].get(img_filename,False)):
+            return self._filename
+
+        if manifest != None and isinstance(img_filename,str):
+            manifest['data'][img_filename]={"img_path":img_path}
+
+        return self.write(manifest)
 
 
     '''
@@ -18,19 +84,15 @@ class ImgManifest():
     '''
     def create(self):
 
+        manifest_filename = None
+        manifest_skeleton = {'data':{},'checksum':'0'}
 
-        manifest_filename = "{}/{}.json".format(self._CONFIG_PATH,self._id)
-        manifest_skeleton = {'data':[],'checksum':'0'}
-
-        if not self._manifest_exists(manifest_filename):
-            try:
-                with open(manifest_filename,"w") as manifestFile:
-                    json.dump(str(manifest_skeleton),manifestFile)
-
-            except EnvironmentError:
-                manifest_filename = None
+        if not self._manifest_exists(self._filename):
+            self.write(manifest_skeleton)
+            manifest_filename = self._filename
 
         return manifest_filename
+
 
 
     def _manifest_exists(self,filename):
@@ -58,5 +120,8 @@ def test_func():
 
 
 if __name__ == "__main__":
-    imgm = ImgManifest("i001234")
+    imgm = ImgManifest("frame1")
     print(imgm.create())
+    print(imgm)
+    print(imgm.add_img("newPhoto1.png"))
+    print(imgm)
